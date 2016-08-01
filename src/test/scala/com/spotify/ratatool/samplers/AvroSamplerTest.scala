e4079aceb5e45a59d75dd4a1d45c67c378faa6d1
Neville Li <neville@spotify.com>
2016-08-01 16:15:25 -0400

    add AvroSamplerTest

---
+++ src/test/scala/com/spotify/ratatool/samplers/AvroSamplerTest.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.ratatool.samplers
+
+import java.io.File
+import java.nio.file.Files
+
+import com.spotify.ratatool.Schemas
+import com.spotify.ratatool.generators.AvroGenerator
+import com.spotify.ratatool.io.AvroIO
+import org.apache.hadoop.fs.Path
+import org.scalatest.{FlatSpec, Matchers}
+
+class AvroSamplerTest extends FlatSpec with Matchers {
+
+  val schema = Schemas.avroSchema
+  val data1 = (1 to 40000).map(_ => AvroGenerator.avroOf(schema))
+  val data2 = (1 to 10000).map(_ => AvroGenerator.avroOf(schema))
+
+  val dir = Files.createTempDirectory("ratatool-")
+  val file1 = new File(dir.toString, "part-00000.avro")
+  val file2 = new File(dir.toString, "part-00001.avro")
+
+  AvroIO.writeToFile(data1, schema, file1)
+  AvroIO.writeToFile(data2, schema, file2)
+
+  "AvroSampler" should "support single file in head mode" in {
+    val result = new AvroSampler(new Path(file1.getAbsolutePath)).sample(10, head = true)
+    result.size shouldBe 10
+    result should equal (data1.take(10))
+  }
+
+  it should "support single file in random mode" in {
+    val result = new AvroSampler(new Path(file1.getAbsolutePath)).sample(10, head = false)
+    result.size shouldBe 10
+    result.forall(data1.contains(_)) shouldBe true
+  }
+
+  it should "support multiple files in head mode" in {
+    val result = new AvroSampler(new Path(dir.toString)).sample(10, head = true)
+    result.size shouldBe 10
+    result should equal (data1.take(10))
+  }
+
+  it should "support multiple files in random mode" in {
+    val result = new AvroSampler(new Path(dir.toString)).sample(10, head = false)
+    result.size shouldBe 10
+    result.exists(data1.contains(_)) shouldBe true
+    result.exists(data2.contains(_)) shouldBe true
+    result.forall(data1.contains(_)) shouldBe false
+    result.forall(data2.contains(_)) shouldBe false
+    data1.count(result.contains(_)) should be > data2.count(result.contains(_))
+  }
+
+}
