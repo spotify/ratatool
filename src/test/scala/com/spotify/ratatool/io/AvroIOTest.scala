90bbf95559373672f47a01ade8a49ff420942de0
Neville Li <neville@spotify.com>
2016-08-01 15:22:31 -0400

    add IO tests

---
+++ src/test/scala/com/spotify/ratatool/io/AvroIOTest.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.ratatool.io
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File}
+
+import com.spotify.ratatool.Schemas
+import com.spotify.ratatool.generators.AvroGenerator
+import org.scalatest.{FlatSpec, Matchers}
+
+class AvroIOTest extends FlatSpec with Matchers {
+
+  val schema = Schemas.avroSchema
+  val data = (1 to 100).map(_ => AvroGenerator.avroOf(schema))
+
+  "AvroIO" should "work with stream" in {
+    val out = new ByteArrayOutputStream()
+    AvroIO.writeToOutputStream(data, schema, out)
+    val in = new ByteArrayInputStream(out.toByteArray)
+    val result = AvroIO.readFromInputStream(in).toList
+    result should equal (data)
+  }
+
+  it should "work with file" in {
+    val file = File.createTempFile("ratatool-", ".avro")
+    AvroIO.writeToFile(data, schema, file)
+    val result = AvroIO.readFromFile(file).toList
+    result should equal (data)
+  }
+
+}
---
+++ src/test/scala/com/spotify/ratatool/io/TableRowJsonIOTest.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.ratatool.io
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File}
+
+import com.spotify.ratatool.Schemas
+import com.spotify.ratatool.generators.TableRowGenerator
+import org.scalatest.{FlatSpec, Matchers}
+
+class TableRowJsonIOTest extends FlatSpec with Matchers {
+
+  val schema = Schemas.tableSchema
+  val data = (1 to 100).map(_ => TableRowGenerator.tableRowOf(schema))
+
+  "TableRowJsonIO" should "work with stream" in {
+    val out = new ByteArrayOutputStream()
+    TableRowJsonIO.writeToOutputStream(data, out)
+    val in = new ByteArrayInputStream(out.toByteArray)
+    val result = TableRowJsonIO.readFromInputStream(in).toList.map(_.toString)
+    result should equal (data.map(_.toString))
+  }
+
+  it should "work with file" in {
+    val file = File.createTempFile("ratatool-", ".json")
+    TableRowJsonIO.writeToFile(data, file)
+    val result = TableRowJsonIO.readFromFile(file).toList.map(_.toString)
+    result should equal (data.map(_.toString))
+  }
+
+}
