449f8f522b903a4de7e163583c5de67ab51e91a7
Neville Li <neville@spotify.com>
2016-09-02 16:17:16 -0400

    add AvroDiffy

---
+++ src/main/scala/com/spotify/ratatool/diffy/AvroDiffy.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.ratatool.diffy
+
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericRecord
+
+import scala.collection.JavaConverters._
+
+object AvroDiffy {
+
+  def apply(x: GenericRecord, y: GenericRecord): Seq[String] = {
+    require(x.getSchema == y.getSchema)
+    diff(x, y, "")
+  }
+
+  private def diff(x: GenericRecord, y: GenericRecord, root: String): Seq[String] = {
+    x.getSchema.getFields.asScala.flatMap { f =>
+      val name = f.name()
+      val fullName = if (root.isEmpty) name else root + "." + name
+      val schema = getRawType(f.schema())
+      schema.getType match {
+        case Schema.Type.RECORD =>
+          val a = x.get(name).asInstanceOf[GenericRecord]
+          val b = y.get(name).asInstanceOf[GenericRecord]
+          if (a != b && (a == null || b == null)) {
+            Seq(fullName)
+          } else {
+            diff(a, b, fullName)
+          }
+        case _ =>
+          if (x.get(name) == y.get(name)) None else Seq(fullName)
+      }
+    }
+  }
+
+  private def getRawType(schema: Schema): Schema = {
+    schema.getType match {
+      case Schema.Type.UNION =>
+        assert(schema.getTypes.size == 2 && schema.getTypes.get(0).getType == Schema.Type.NULL)
+        schema.getTypes.get(1)
+      case _ => schema
+    }
+  }
+
+}
---
+++ src/test/scala/com/spotify/ratatool/diffy/AvroDiffyTest.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.ratatool.diffy
+
+import com.google.cloud.dataflow.sdk.coders.AvroCoder
+import com.google.cloud.dataflow.sdk.util.CoderUtils
+import com.spotify.ratatool.avro.specific.{NullableNestedRecord, TestRecord}
+import com.spotify.ratatool.generators.AvroGenerator
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.JavaConverters._
+
+class AvroDiffyTest extends FlatSpec with Matchers {
+
+  def jl[T](x: T*): java.util.List[T] = List(x: _*).asJava
+
+  "AvroDiffy" should "support primitive fields" in {
+    val x = NullableNestedRecord.newBuilder().setIntField(10).setLongField(20L).build()
+    val y = NullableNestedRecord.newBuilder().setIntField(10).setLongField(20L).build()
+    val z = NullableNestedRecord.newBuilder().setIntField(10).setLongField(200L).build()
+
+    AvroDiffy(x, y) should equal (Nil)
+    AvroDiffy(x, z) should equal (Seq("long_field"))
+  }
+
+  it should "support nested fields" in {
+    val coder = AvroCoder.of(classOf[TestRecord])
+
+    val x = AvroGenerator.avroOf[TestRecord]
+    x.getNullableFields.setIntField(10)
+    x.getNullableFields.setLongField(20L)
+    x.getNullableFields.setStringField("hello")
+
+    val y = CoderUtils.clone(coder, x)
+    val z = CoderUtils.clone(coder, x)
+    z.getNullableFields.setLongField(200L)
+    z.getNullableFields.setStringField("world")
+
+    AvroDiffy(x, y) should equal (Nil)
+    AvroDiffy(x, z) should equal (Seq("nullable_fields.long_field", "nullable_fields.string_field"))
+  }
+
+  it should "support repeated fields" in {
+    val coder = AvroCoder.of(classOf[TestRecord])
+
+    val x = AvroGenerator.avroOf[TestRecord]
+    x.getRepeatedFields.setIntField(jl(10, 11))
+    x.getRepeatedFields.setLongField(jl(20L, 21L))
+    x.getRepeatedFields.setStringField(jl("hello", "world"))
+
+    val y = CoderUtils.clone(coder, x)
+    val z = CoderUtils.clone(coder, x)
+    z.getRepeatedFields.setLongField(jl(20L, 210L))
+    z.getRepeatedFields.setStringField(jl("Hello", "World"))
+
+    AvroDiffy(x, y) should equal (Nil)
+    AvroDiffy(x, z) should equal (Seq("repeated_fields.long_field", "repeated_fields.string_field"))
+  }
+
+}
