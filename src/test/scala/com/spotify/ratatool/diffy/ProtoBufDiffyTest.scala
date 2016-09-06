503b98c389e1d0bc84c5bc2f095222e38398a7c8
Neville Li <neville@spotify.com>
2016-09-06 13:49:56 -0400

    add ProtoBufDiffy

---
+++ src/test/scala/com/spotify/ratatool/diffy/ProtoBufDiffyTest.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2016 Spotify AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.ratatool.diffy
+
+import com.spotify.ratatool.generators.ProtoBufGenerator
+import com.spotify.ratatool.proto.Schemas.{OptionalNestedRecord, RepeatedNestedRecord, TestRecord}
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.JavaConverters._
+
+class ProtoBufDiffyTest extends FlatSpec with Matchers {
+
+  def jl[T](x: T*): java.util.List[T] = List(x: _*).asJava
+
+  "ProtoBufDiffy" should "support primitive fields" in {
+    val x = OptionalNestedRecord.newBuilder().setInt32Field(10).setInt64Field(20L).build()
+    val y = OptionalNestedRecord.newBuilder().setInt32Field(10).setInt64Field(20L).build()
+    val z = OptionalNestedRecord.newBuilder().setInt32Field(10).setInt64Field(200L).build()
+
+    ProtoBufDiffy(x, y) should equal (Nil)
+    ProtoBufDiffy(x, z) should equal (Seq(Delta("int64_field", 20L, 200L)))
+  }
+
+  it should "support nested fields" in {
+    val base = ProtoBufGenerator.protoBufOf[TestRecord]
+    val x = TestRecord.newBuilder(base)
+      .setOptionalFields(
+        OptionalNestedRecord.newBuilder(base.getOptionalFields)
+          .setInt32Field(10)
+          .setInt64Field(20L)
+          .setStringField("hello")
+          .build()
+      ).build()
+    val y = TestRecord.newBuilder(x).build()
+    val z = TestRecord.newBuilder(x)
+      .setOptionalFields(
+        OptionalNestedRecord.newBuilder(x.getOptionalFields)
+          .setInt64Field(200L)
+          .setStringField("world")
+      ).build()
+
+    ProtoBufDiffy(x, y) should equal (Nil)
+    ProtoBufDiffy(x, z) should equal (Seq(
+      Delta("optional_fields.int64_field", 20L, 200L),
+      Delta("optional_fields.string_field", "hello", "world")))
+  }
+
+
+  it should "support repeated fields" in {
+    val base = ProtoBufGenerator.protoBufOf[TestRecord]
+    val x = TestRecord.newBuilder(base)
+      .setRepeatedFields(
+        RepeatedNestedRecord.newBuilder(base.getRepeatedFields)
+          .clearInt32Field()
+          .clearInt64Field()
+          .clearStringField()
+          .addAllInt32Field(jl(10, 11))
+          .addAllInt64Field(jl(20L, 21L))
+          .addAllStringField(jl("hello", "world"))
+          .build()
+      ).build()
+    val y = TestRecord.newBuilder(x).build()
+    val z = TestRecord.newBuilder(x)
+      .setRepeatedFields(
+        RepeatedNestedRecord.newBuilder(x.getRepeatedFields)
+          .clearInt64Field()
+          .clearStringField()
+          .addAllInt64Field(jl(20L, 210L))
+          .addAllStringField(jl("Hello", "World"))
+          .build()
+      ).build()
+
+    ProtoBufDiffy(x, y) should equal (Nil)
+    ProtoBufDiffy(x, z) should equal (Seq(
+      Delta("repeated_fields.int64_field", jl(20L, 21L), jl(20L, 210L)),
+      Delta("repeated_fields.string_field", jl("hello", "world"), jl("Hello", "World"))))
+  }
+
+}
