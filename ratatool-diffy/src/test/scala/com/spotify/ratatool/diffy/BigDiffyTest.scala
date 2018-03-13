f5c8aa4835c9f27c07a746c3ba414e518f30c87e
Idrees Khan <idrees@spotify.com>
2018-03-13 13:52:18 -0400

    Reduce amount of generated data for BigDiffyTest to consume less mem

--- ratatool-diffy/src/test/scala/com/spotify/ratatool/diffy/BigDiffyTest.scala
+++ ratatool-diffy/src/test/scala/com/spotify/ratatool/diffy/BigDiffyTest.scala
@@ -27,7 +27,7 @@ import org.scalacheck.Gen
 
 class BigDiffyTest extends PipelineSpec {
 
-  val keys = (1 to 1000).map("key" + _)
+  val keys = (1 to 500).map("key" + _)
   val coder = AvroCoder.of(classOf[TestRecord])
 
   /** Fixed to a small range so that Std. Dev. & Variance calculations are easier to predict */
@@ -35,7 +35,7 @@ class BigDiffyTest extends PipelineSpec {
     .amend(Gen.choose[Double](0.0, 1.0))(_.setDoubleField)
   val specificGen = specificRecordOf[TestRecord]
     .amend(rnr)(_.setRequiredFields)
-  val lhs = Gen.listOfN(1000, specificGen).sample.get.zip(1 to 1000)
+  val lhs = Gen.listOfN(500, specificGen).sample.get.zip(1 to 500)
     .map { case (r, i) =>
       r.getRequiredFields.setIntField(i)
       r.getRequiredFields.setStringField(new Utf8("key" + i))
@@ -49,7 +49,7 @@ class BigDiffyTest extends PipelineSpec {
       val result = BigDiffy.diff[TestRecord](
         sc.parallelize(lhs), sc.parallelize(rhs),
         new AvroDiffy[TestRecord](), _.getRequiredFields.getStringField.toString)
-      result.globalStats should containSingleValue (GlobalStats(1000L, 1000L, 0L, 0L, 0L))
+      result.globalStats should containSingleValue (GlobalStats(500L, 500L, 0L, 0L, 0L))
       result.deltas should beEmpty
       result.keyStats should containInAnyOrder (keys.map(KeyStats(_, DiffType.SAME)))
       result.fieldStats should beEmpty
@@ -65,47 +65,47 @@ class BigDiffyTest extends PipelineSpec {
       val result = BigDiffy.diff[TestRecord](
         sc.parallelize(lhs), sc.parallelize(rhs),
         new AvroDiffy[TestRecord](), _.getRequiredFields.getStringField.toString)
-      result.globalStats should containSingleValue (GlobalStats(1000L, 0L, 1000L, 0L, 0L))
+      result.globalStats should containSingleValue (GlobalStats(500L, 0L, 500L, 0L, 0L))
       result.deltas.map(d => (d._1, d._2)) should containInAnyOrder (
         keys.map((_, field)))
       result.keyStats should containInAnyOrder (keys.map(KeyStats(_, DiffType.DIFFERENT)))
       result.fieldStats.map(f => (f.field, f.count, f.fraction)) should containSingleValue (
-        (field, 1000L, 1.0))
+        (field, 500L, 1.0))
       // Double.NaN comparison is always false
       val deltaStats = result.fieldStats
         .flatMap(_.deltaStats)
         .map(d => (d.deltaType, d.min, d.max, d.count, d.mean, d.variance, d.stddev))
-      deltaStats should containSingleValue ((DeltaType.NUMERIC, 10.0, 10.0, 1000L, 10.0, 0.0, 0.0))
+      deltaStats should containSingleValue ((DeltaType.NUMERIC, 10.0, 10.0, 500L, 10.0, 0.0, 0.0))
     }
   }
 
   it should "work with missing LHS" in {
     runWithContext { sc =>
-      val lhs2 = lhs.filter(_.getRequiredFields.getIntField <= 500)
+      val lhs2 = lhs.filter(_.getRequiredFields.getIntField <= 250)
       val rhs = lhs.map(CoderUtils.clone(coder, _))
       val result = BigDiffy.diff[TestRecord](
         sc.parallelize(lhs2), sc.parallelize(rhs),
         new AvroDiffy[TestRecord](), _.getRequiredFields.getStringField.toString)
-      result.globalStats should containSingleValue (GlobalStats(1000L, 500L, 0L, 500L, 0L))
+      result.globalStats should containSingleValue (GlobalStats(500L, 250L, 0L, 250L, 0L))
       result.deltas should beEmpty
       result.keyStats should containInAnyOrder (
-        (1 to 500).map(i => KeyStats("key" + i, DiffType.SAME)) ++
-          (501 to 1000).map(i => KeyStats("key" + i, DiffType.MISSING_LHS)))
+        (1 to 250).map(i => KeyStats("key" + i, DiffType.SAME)) ++
+          (251 to 500).map(i => KeyStats("key" + i, DiffType.MISSING_LHS)))
       result.fieldStats should beEmpty
     }
   }
 
   it should "work with missing RHS" in {
     runWithContext { sc =>
-      val rhs = lhs.filter(_.getRequiredFields.getIntField <= 500).map(CoderUtils.clone(coder, _))
+      val rhs = lhs.filter(_.getRequiredFields.getIntField <= 250).map(CoderUtils.clone(coder, _))
       val result = BigDiffy.diff[TestRecord](
         sc.parallelize(lhs), sc.parallelize(rhs),
         new AvroDiffy[TestRecord](), _.getRequiredFields.getStringField.toString)
-      result.globalStats should containSingleValue (GlobalStats(1000L, 500L, 0L, 0L, 500L))
+      result.globalStats should containSingleValue (GlobalStats(500L, 250L, 0L, 0L, 250L))
       result.deltas should beEmpty
       result.keyStats should containInAnyOrder (
-        (1 to 500).map(i => KeyStats("key" + i, DiffType.SAME)) ++
-          (501 to 1000).map(i => KeyStats("key" + i, DiffType.MISSING_RHS)))
+        (1 to 250).map(i => KeyStats("key" + i, DiffType.SAME)) ++
+          (251 to 500).map(i => KeyStats("key" + i, DiffType.MISSING_RHS)))
       result.fieldStats should beEmpty
     }
   }
